ed620a509cbb62e161de5a97fca9374fb2de30c3
SHRUTHIMANOJKUMAR <shruthih0111@gmail.com>
2025-03-11 14:50:41 +0530

    my 1st commit

---
+++ src/test/java/Package1/Demo.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void getdata() {
+		
+	}
+}
---
+++ src/test/java/Package2/OrgTest.java
@@ -0,0 +1,16 @@
+package Package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgtest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
